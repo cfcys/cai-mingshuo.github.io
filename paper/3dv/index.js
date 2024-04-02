51eb9733013910c49d9ea00ed10b06ff4d854c0f
明硕 <91956930+cfcys@users.noreply.github.com>
2024-04-02 22:48:56 +0800

    111

---
+++ paper/3dv/index.js
@@ -0,0 +1,34 @@
+import * as SPLAT from "https://cdn.jsdelivr.net/npm/gsplat@latest";
+
+const canvas = document.getElementById("canvas");
+const progressDialog = document.getElementById("progress-dialog");
+const progressIndicator = document.getElementById("progress-indicator");
+
+const renderer = new SPLAT.WebGLRenderer(canvas);
+const scene = new SPLAT.Scene();
+const camera = new SPLAT.Camera();
+const controls = new SPLAT.OrbitControls(camera, canvas);
+
+async function main() {
+    // Load and convert ply from url
+    const url =
+        "https://huggingface.co/datasets/dylanebert/3dgs/resolve/main/bonsai/point_cloud/iteration_7000/point_cloud.ply";
+    // const url = "model/door.ply"
+    // const url = "model/LB.ply"
+    // const url = "model/point_cloud.ply"
+    await SPLAT.PLYLoader.LoadAsync(url, scene, (progress) => (progressIndicator.value = progress * 100));
+    progressDialog.close();
+    scene.saveToFile("bonsai.splat");
+
+    // Render loop
+    const frame = () => {
+        controls.update();
+        renderer.render(scene, camera);
+
+        requestAnimationFrame(frame);
+    };
+
+    requestAnimationFrame(frame);
+}
+
+main();
