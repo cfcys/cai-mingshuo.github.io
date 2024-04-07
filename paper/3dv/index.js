dc45d73037fe0fc67ae8ada8bc513a7d7761dc1e
明硕 <91956930+cfcys@users.noreply.github.com>
2024-04-07 21:48:16 +0800

    111

--- paper/3dv/index.js
+++ paper/3dv/index.js
@@ -11,7 +11,7 @@ const controls = new SPLAT.OrbitControls(camera, canvas);
 
 async function main() {
     // const url = "https://huggingface.co/datasets/dylanebert/3dgs/resolve/main/bonsai/bonsai-7k-mini.splat";
-    const url = "https://huggingface.co/datasets/Cfcys/gsplat_view/resolve/main/door.splat"
+    const url = "https://huggingface.co/datasets/Cfcys/gsplat_view/resolve/main/LB.splat"
 
     await SPLAT.Loader.LoadAsync(url, scene, (progress) => progressIndicator.value = progress * 100);
     progressDialog.close();
