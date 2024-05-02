debde7a16bbe445ed9d80cddbdda7a2d4c84d9c0
明硕 <91956930+cfcys@users.noreply.github.com>
2024-05-02 10:59:33 +0800

    111

---
+++ paper/3dv4/index.js
@@ -0,0 +1,38 @@
+import * as SPLAT from "https://cdn.jsdelivr.net/npm/gsplat@latest";
+
+const canvas = document.getElementById("canvas");
+const progressDialog = document.getElementById("progress-dialog");
+const progressIndicator = document.getElementById("progress-indicator");
+
+const renderer = new SPLAT.WebGLRenderer(canvas);
+const scene = new SPLAT.Scene();
+const camera = new SPLAT.Camera();
+const controls = new SPLAT.OrbitControls(camera, canvas);
+
+async function main() {
+    // const url = "https://huggingface.co/datasets/dylanebert/3dgs/resolve/main/bonsai/bonsai-7k-mini.splat";
+    const url = "https://huggingface.co/datasets/Cfcys/gsplat_view/resolve/main/LB.splat"
+
+    // const url = "model/cloud3.splat"
+
+    await SPLAT.Loader.LoadAsync(url, scene, (progress) => progressIndicator.value = progress * 100);
+    progressDialog.close();
+
+    const handleResize = () => {
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    };
+
+    const frame = () => {
+        controls.update();
+        renderer.render(scene, camera);
+
+        requestAnimationFrame(frame);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    requestAnimationFrame(frame);
+}
+
+main();
